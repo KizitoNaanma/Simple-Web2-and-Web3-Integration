a10f66879071df21776e35e1f03b529aa4912255
KizitoNaanma <naanma4kizito@gmail.com>
2024-11-07 04:10:49 +0100

    fix: deployment

--- dist/utils/environment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loadEnvironment = void 0;
-const dotenv_1 = require("dotenv");
-const loadEnvironment = () => {
-    (0, dotenv_1.config)();
-};
-exports.loadEnvironment = loadEnvironment;
-//# sourceMappingURL=environment.js.map
